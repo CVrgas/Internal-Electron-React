95446c25436956c18c08d70a23cd34ba1b62085d
CVrgas <usual389@gmail.com>
2023-08-27 17:05:21 -0400

    empty

--- public/preload.js
+++
@@ -1,7 +0,0 @@
-const { ipcRenderer, contextBridge } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-	getCoinsBy: ({ id }) => ipcRenderer.invoke("get-coins-By", { id }),
-	saveCoin: (coin) => ipcRenderer.invoke("save-coin", coin),
-	getAllCoins: ({ option }) => ipcRenderer.invoke("get-all-coins", { option }),
-});
